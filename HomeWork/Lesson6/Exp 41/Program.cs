68c0bc79221a369e8b02a4f7c1cca8e8dc4a2603
Levchenko Vladislav <levchenko.vg@gmail.com>
2022-09-08 02:34:19 +0300

    Домашка №6

---
+++ HomeWork/Lesson6/Exp 41/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] ArrayNumbers = new int[10000000];
+int Result = 0;
+
+int GetNumbers()
+{
+    int i = 0;
+    int a = 0;
+    int a_print = 0;
+    string value_ = string.Empty;    
+    while (i < 1)
+    {
+        a_print = a + 1;
+        if (a == 0) {Console.Write($"Введите {a_print} число: ");}
+        else {Console.Write($"Введите {a_print} число или для запуска вычисления введите 'go': ");}        
+        value_ = Console.ReadLine();
+        if (value_ == "go") break;
+        else ArrayNumbers[a] = Convert.ToInt32(value_);
+        a++;
+    }
+    a_print--;
+    return a_print;
+}
+
+int CountNumbers(int MaxArrayCount) {    
+    for(int i = 0; i < MaxArrayCount; i++){
+        if(ArrayNumbers[i] > 0) Result++;        
+    }
+    return Result;
+}
+
+void PrintResult (int Result){
+    Console.Write(Result);
+}
+
+void Main(){
+    int MaxArrayCount = GetNumbers(); 
+    PrintResult(CountNumbers(MaxArrayCount));
+}
+
+Main();
---
+++ HomeWork/Lesson6/Exp 43/Program.cs
@@ -0,0 +1,57 @@
+﻿double[,] Params = new double[2, 2];
+double[] Intersect = new double[2];
+
+void GetParams()
+{
+    int count = 0;
+    for (int i = 0; i < Params.GetLength(0); i++)
+    {
+        count = i + 1;
+        Console.WriteLine($"Введите переменные для {count} уравнения y = k{count} * x + b{count}");
+        for (int j = 0; j < Params.GetLength(1); j++)
+        {
+            if (j == 0)
+            {
+                Console.Write($"Переменная b{count}: ");
+            }
+            else
+            {
+                Console.Write($"Переменная k{count}: ");
+            }
+            Params[i, j] = Convert.ToDouble(Console.ReadLine());
+        }
+    }    
+}
+
+double[] GetСalculation(double[,] ArrayParams){
+  Intersect[0] = (ArrayParams[0,0] - ArrayParams[1,0]) / (ArrayParams[1,1] - ArrayParams[0,1]);
+  Intersect[1] = ArrayParams[0,1] * Intersect[0] + ArrayParams[0,0];
+  return Intersect;
+}
+
+void GetPrint() {
+  double[] Result = new double[2];
+  Result = GetСalculation(Params);
+  Console.WriteLine("Точка пересечения [{0}]", string.Join(",", Result));
+}
+
+void Main(){
+  int i = 0;
+  GetParams();
+  while( i < 1){
+    if(Params[0,0] == Params[1,0] && Params[0,1] == Params[1,1]){
+      Console.WriteLine("Прямые совпадают");
+      break;
+    }
+    else if(Params[0,0] != Params[1,0] && Params[0,1] == Params[1,1]){    
+      Console.WriteLine("Прямые параллельны");
+      break;
+    }
+    else {
+      GetPrint();
+      break;    
+    }
+  }
+}
+
+Main();
