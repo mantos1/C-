36f50be271acaa89fc2ce2fe1c93ea825a09340b
Levchenko Vladislav <levchenko.vg@gmail.com>
2022-09-15 11:09:28 +0300

    Корректировка 60 упражнения в ДЗ №8

--- HomeWork/Lesson8/Exp 60/Program.cs
+++ HomeWork/Lesson8/Exp 60/Program.cs
@@ -2,13 +2,29 @@
 {
     double[,,] Array = new double[x, y, z];
     Random Rand = new Random();
+    double[] ArrayRandomNumbers = new double[x*y*z];
+    for(int i = 0; i < x*y*z; i++){
+        double Rnd = Rand.Next(10, 100);
+        int j = 0;
+        while(j < x*y*z){
+            if(ArrayRandomNumbers[j] == Rnd){
+                Rnd = Rand.Next(10, 100);
+                j = 0;
+            }else j++;
+        }
+        ArrayRandomNumbers[i] = Rnd;
+        //Console.WriteLine(Rnd);
+    }
+    int a = 0;
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             for (int n = 0; n < z; n++)
             {
-                Array[i, j, n] = Rand.Next(1, 10);
+              
+                Array[i, j, n] = ArrayRandomNumbers[a];
+                a++;
             }
         }
     }
